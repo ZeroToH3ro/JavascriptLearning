2258faad060ac9fd663a7005405e258c997ef19c
ZeroToH3ro <mtblaser2002@gmail.com>
2023-07-05 14:02:32 +0700

    practice make perfect

--- SelfLearning/NodeJSSelf/practice-ongdev/src/controllers/home.controller.js
+++ SelfLearning/NodeJSSelf/practice-ongdev/src/controllers/home.controller.js
@@ -14,10 +14,16 @@ let detailPage = async (req, res) => {
     try{
         const requestID = req.params.id;
         const result = await connectDB.query('SELECT * FROM users WHERE id = $1', [requestID]);
-        return res.status(200).json({
-            status: "get detail page success",
-            data: result.rows[0]
-        });
+        if(result.rows[0]){
+            return res.status(200).json({
+                status: "get detail page success",
+                data: result.rows[0]
+            });
+        } else {
+            return res.status(400).json({
+                status: "fail"
+            })
+        }
     } catch (e) {
         console.log(e);
     }
@@ -35,8 +41,70 @@ let createNewUser = async (req, res) => {
         console.log(e);
     }
 }
+
+let editUser = async (req, res) => {
+    try{
+        const result = await connectDB.query('UPDATE users SET firstname = $1, lastname = $2, email = $3, address = $4 WHERE id = $5 RETURNING *',
+            [req.body.firstname, req.body.lastname, req.body.email, req.body.address, req.params.id]);
+
+        if(result.rows[0]){
+            return res.status(200).json({
+                status: "update user success",
+                data: result.rows[0],
+            })
+        } else {
+            return res.status(400).json({
+                status: "fail"
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+let deleteUser = async (req, res) => {
+    try{
+        const result = await connectDB.query('DELETE FROM users WHERE id = $1 RETURNING *', [req.params.id]);
+
+        if(result.rows[0]){
+            res.status(200).json({
+                status: "delete user success",
+                data: result.rows[0]
+            })
+            return res.redirect('/');
+        } else {
+            return res.status(400).json({
+                status: "fail"
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+let uploadFilePage = async  (req, res) => {
+    return res.render('uploadFile.ejs');
+}
+
+let handleUploadFile = async (req, res) => {
+
+    if (req.fileValidationError) {
+
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+};
+
 module.exports = {
     getHomePages,
     detailPage,
-    createNewUser
+    createNewUser,
+    editUser,
+    deleteUser,
+    uploadFilePage,
+    handleUploadFile,
 }
--- SelfLearning/NodeJSSelf/practice-ongdev/src/routes/web.js
+++ SelfLearning/NodeJSSelf/practice-ongdev/src/routes/web.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const path = require('path');
-const { getHomePages, detailPage, createNewUser} = require('../controllers/home.controller');
+const multer = require('multer');
+const appRoot = require('app-root-path');
+const { getHomePages, detailPage, createNewUser, editUser, deleteUser,  uploadFilePage, handleUploadFile} = require('../controllers/home.controller');
 
 let router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
+// let uploadMultipleFiles = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images', 3);
 
 const initWebRouter = (app) => {
     router.get('/', getHomePages);
     router.get('/detail-user/:id', detailPage);
-    router.post('/create-user', createNewUser);
+    router.post('/create-new-user', createNewUser);
+    router.post('/edit-user/:id', editUser);
+    router.post('/delete-user/:id', deleteUser);
 
+    router.get('/upload', uploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), handleUploadFile)
     return app.use('/', router);
 }
 
